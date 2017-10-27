7d65df083e46f8f8c89446b766d448e9ea9bc89c
Karthik M S <ms18karthik@gmail.com>
2017-10-27 16:34:11 +0530

    Added Comments controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'demo/index'
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
 
   resources :blogs
+
   match ':controller(/:action(/:id))', :via => :get
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026183238) do
+ActiveRecord::Schema.define(version: 20171027100507) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20171026183238) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "posts", force: :cascade do |t|
